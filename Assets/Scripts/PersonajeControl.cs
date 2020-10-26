6577bed879d94a6484c94439bbec17bf7e53f964
Juan I. Lanosa <blackmoodgames@gmail.com>
2020-10-25 21:53:52 -0300

    camara, movimiento y algo de interaccion

--- Assets/Scripts/PersonajeControl.cs
+++ Assets/Scripts/PersonajeControl.cs
@@ -5,70 +5,75 @@ using UnityEngine;
 public class PersonajeControl : MonoBehaviour
 {
 
+    public float multipSprint = 1.9f;
     public float velocidadMovimiento = 5.0f;
     public float velocidadRotacion = 200.0f;
-    public Animator anim;
+
+    private Animator anim;
+    private Rigidbody rbPlayer;
+
     public float x, y;
 
-    public bool tocandoRecuerdo;
-    public bool avanzarSolo;
+    //public bool tocandoRecuerdo;
+    //public bool avanzarSolo;
+    // ^ esto para que querian usarlo?
 
     void Start()
     {
 
         anim = GetComponent<Animator>();
+        rbPlayer = GetComponent<Rigidbody>();
 
     }
 
 
-    void FixedUpdate ()
+    void FixedUpdate()
     {
-        if(!tocandoRecuerdo)
-        {
-            transform.Rotate(0, x * Time.deltaTime * velocidadRotacion, 0);
+        
+        transform.Rotate(0, x * Time.deltaTime * velocidadRotacion, 0);
+        rbPlayer.MovePosition(transform.position + transform.forward * y * Time.deltaTime * velocidadMovimiento);
+        
+        #region Viejo
+        //if(!tocandoRecuerdo)
+        //{
 
-            transform.Translate(0, 0, y * Time.deltaTime * velocidadMovimiento);
+        //transform.Translate(0, 0, y * Time.deltaTime * velocidadMovimiento);
 
-        }
+        //}
 
-      /*  if(avanzarSolo)
-        {
-            rb.velocicity = transform.forward
-        }*/
+        /*  if(avanzarSolo)
+          {
+              rb.velocicity = transform.forward
+          }*/
+        #endregion
 
     }
 
 
     void Update()
     {
+        x = Input.GetAxis("Horizontal") ;
 
-        x = Input.GetAxis("Horizontal");
-
-        y = Input.GetAxis("Vertical");
-
-        if(Input.GetKeyDown(KeyCode.Return) && !tocandoRecuerdo)
-        {
-            anim.SetTrigger("tocar");
-            tocandoRecuerdo = true;
-        }
-
-        /*transform.Rotate(0, x * Time.deltaTime * velocidadRotacion, 0);
-
-        transform.Translate(0, 0, y * Time.deltaTime * velocidadMovimiento);*/
-       
+        var sprintVal = Input.GetAxis("Sprint") != 0 ? multipSprint : 1;
+        y = Input.GetAxis("Vertical") * sprintVal; // movimiento * correr
 
         anim.SetFloat("VelX", x);
         anim.SetFloat("VelY", y);
 
-       }
+        if (sprintVal != 1) // muuy primitivo pero ahora nos sirve para usar la misma animacion de caminar
+            this.anim.speed = 1.3f;
+        else
+            this.anim.speed = 1;
 
-    public void DejoDeTocar()
-    {
-        tocandoRecuerdo = false;
     }
 
-    public void DejoDeAvanzar()
-    {
-        avanzarSolo = false;
-    }
+    //public void DejoDeTocar()
+    //{
+    //    tocandoRecuerdo = false;
+    //}
+
+    //public void DejoDeAvanzar()
+    //{
+    //    avanzarSolo = false;
+    //}
 }
---
+++ Assets/Scripts/PlayerControlCC.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlCC : MonoBehaviour
+{
+    public Animator anim;
+
+    public CharacterController controller;
+    public Transform cam;
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+
+    public float multipSprint = 1.9f;
+
+
+    private float turnSmoothVel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        
+        var sprintVal = Input.GetAxis("Sprint") != 0 ? multipSprint : 1;
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+        if(direction.magnitude >= 0.1f) // check constante
+        {
+            
+
+            if (sprintVal != 1) // muuy primitivo pero ahora nos sirve para usar la misma animacion de caminar
+                this.anim.speed = 1.3f;
+            else
+                this.anim.speed = 1;
+
+            //calculo la rotacion que tiene que hacer el personaje
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y; 
+            // angulo + offset basado en como esta la camara
+            
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVel, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            //para mover en la direccion correcta
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * sprintVal * Time.deltaTime);
+        }
+
+        anim.SetFloat("VelX", horizontal);
+        anim.SetFloat("VelY", vertical);
+    }
+}
